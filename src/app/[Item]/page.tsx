c0cb37b6417a561a9915fbd39c55d3c989602a3e
Satyam <satyam@Satyams-MacBook-Air.local>
2024-08-12 23:54:53 +0530

    first commit

---
+++ src/app/[Item]/page.tsx
@@ -0,0 +1,12 @@
+import MessageBox from "@/components/message-box";
+import React from "react";
+
+type Props = {};
+
+export default function ItemPage({}: Props) {
+  return (
+    <div className="h-full">
+      <MessageBox />
+    </div>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import MessageBox from "@/components/message-box";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-full">
+      <MessageBox />
+    </div>
+  );
+}
